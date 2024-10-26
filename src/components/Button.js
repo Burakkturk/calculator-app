234b467016a1304a4072d8af4b9c5fde04838f74
Burakkturk <burak1845turk@icloud.com>
2024-10-26 13:51:47 +0300

    updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,9 @@
+import {useContext} from "react";
+import {CalcContext} from "../context/CalcContext";
+
+
 const getStyleName = btn => {
+
     const className = {
         '=' : 'equals',
         'x' : 'opt',
@@ -9,9 +14,93 @@ const getStyleName = btn => {
     return className[btn]
 }
 
+
 const Button = ({value}) => {
+const {calc, setCalc} = useContext(CalcContext)
+
+
+
+    // user click comma
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    })
+      
+  }
+}
+const resetClick = () => {
+  setCalc({sign: '', num:0, res:0})
+}
+// user click number
+const handleClickButton = () => {
+  const numberString = value.toString()
+  let numberValue;
+if(numberString === '0' && calc.num === 0) {
+  numberValue = "0"
+} else {
+  numberValue = Number(calc.num + numberString)
+}
+setCalc({
+  ...calc,
+  num: numberValue
+})
+// User Click operation
+
+const signClick = () => {
+  setCalc({
+sign: value,
+res: !calc.res && calc.num ? calc.num: calc.res, 
+num: 0
+
+  })
+}
+
+// User Click ewuals
+const equalsClick = () => {
+  const math = (a,b,sign) => {
+    const result = {
+      '+' : (a,b) => a+ b ,
+      '-' : (a,b) => a - b ,
+      'x' : (a,b) => a * b ,
+      '/' : (a,b) => a / b ,
+    }
+  }
+  setCalc({
+    res: Math(calc.re, calc.num, calc.sign),
+    sign: '',
+    num: 0 
+  })
+}
+
+const handleBtnClick = () => {
+
+
+
+  const results = {
+    '.' : commaClick,
+    'C': resetClick,
+    '/': signClick,
+    'x': signClick,
+    '-': signClick,
+    '+': signClick,
+    '=': equalsClick
+  }
+  if (results[value]) {
+return results[value]()
+   } else {
+    return handleClickButton()
+   
+}
+
+
+}
+
+
+
+
   return (
-    <button className={`${getStyleName(value)} button`}>{value}</button>
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
   ) 
 }
 
